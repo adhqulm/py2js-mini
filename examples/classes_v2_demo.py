0fce0244ec07da82170fc37684aae351e2e21c26
Kristina Ganina <adhqulm@gmail.com>
2026-02-12 13:13:34 +0400

    README and fix

--- examples/classes_v2_demo.py
+++ examples/classes_v2_demo.py
@@ -1,16 +1,20 @@
 class A:
     def __init__(self, x):
         self.x = x
+    
     def inc(self, d=1):
         self.x = self.x + d
+
 
 class B(A):
     def __init__(self, x, y):
         super().__init__(x)   # initialize base first
         super().inc(2)        # then bump by 2
         self.y = y
+    
     def add(self):
         return self.x + self.y
+
 
 b = B(5, 10)
 print(b.x, b.y)     # 7 10
